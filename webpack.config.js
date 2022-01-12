de0a3e42728c7f573b57be58ae16841bddaea886
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-12 13:06:08 +0500

    Finished webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry : './src/index.js'
-}
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+         },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, ],
+    },
+};
